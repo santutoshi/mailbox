c35b9730bba2e195b670a5ce56b7991b795d52a9
Santosh <sthapa@devfinity.io>
2023-06-02 15:34:17 +0545

    Service worker implemented and flex design "

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { NgxsModule } from '@ngxs/store';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +8,17 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxsModule.forRoot([])],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxsModule.forRoot([]),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: false,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/layout/content-layout/content-layout.component.ts
+++ src/app/layout/content-layout/content-layout.component.ts
@@ -3,11 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-content-layout',
   template: `
-    <div class="page">
-      <app-nav></app-nav>
-      <main class="content__layout-container">
-        <router-outlet></router-outlet>
-      </main>
+    <div class="row">
+      <div class="col-3">
+        <app-nav></app-nav>
+      </div>
+      <div class="col-9">
+        <main class="content__layout-container">
+          <router-outlet></router-outlet>
+        </main>
+      </div>
     </div>
   `,
 })

09e28b0afd98656a3a5041336024abce2e07509e
Santosh <sthapa@devfinity.io>
2023-06-05 16:29:04 +0545

    Unit test for nav and app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: false,
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000',
+        }),
       ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -30,6 +35,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mailbox app is running!');
+    expect(compiled.querySelector('.col-s-6')?.textContent).toContain(
+      'Status:'
+    );
   });
 });
--- src/app/layout/nav/components/nav/nav.component.spec.ts
+++ src/app/layout/nav/components/nav/nav.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CalculationService } from 'src/app/worker/calculation.service';
 
 import { NavComponent } from './nav.component';
 
@@ -8,9 +11,10 @@ describe('NavComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
-    })
-    .compileComponents();
+      declarations: [NavComponent],
+      imports: [SharedModule],
+      providers: [CalculationService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
@@ -19,5 +23,17 @@ describe('NavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have h1 with text Folders', () => {
+    fixture.detectChanges();
+    let heading = fixture.debugElement.nativeElement.querySelector('h1');
+    expect(heading.textContent).toContain('Folders');
+  });
+
+  it('should have button with text compose', () => {
+    fixture.detectChanges();
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    expect(button.textContent).toContain('Compose');
   });
 });

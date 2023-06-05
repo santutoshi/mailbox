b54de8e79a8d07b0cbfe2fe02a9a8b5acd273ba1
Santosh <sthapa@devfinity.io>
2023-06-05 16:50:29 +0545

    build fixes "

--- src/app/modules/flagged/components/flagged/flagged.component.ts
+++ src/app/modules/flagged/components/flagged/flagged.component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
-import { MessageModel } from 'src/app/modules/inbox/data-access/models/message.model';
-import { GetMailAction } from 'src/app/modules/inbox/data-access/store/inbox-actions';
-import { InboxStateSelector } from 'src/app/modules/inbox/data-access/store/inbox.selector';
+import { Component } from "@angular/core";
+import { Select, Store } from "@ngxs/store";
+import { Observable } from "rxjs";
+import { MessageModel } from "src/app/modules/inbox/data-access/models/message.model";
+import { GetMailAction } from "src/app/modules/inbox/data-access/store/inbox-actions";
+import { InboxStateSelector } from "src/app/modules/inbox/data-access/store/inbox.selector";
 
 @Component({
-  selector: 'app-flagged',
-  templateUrl: './flagged.component.html',
-  styleUrls: ['./flagged.component.scss'],
+  selector: "app-flagged",
+  templateUrl: "./flagged.component.html",
 })
 export class FlaggedComponent {
   constructor(private _store: Store) {}
 
   /** Selects slice of total company state */
-  @Select(InboxStateSelector.SliceOf('mailList'))
+  @Select(InboxStateSelector.SliceOf("mailList"))
   public mailData$!: Observable<Array<MessageModel> | []>;
 
   selectedMailData!: MessageModel;
